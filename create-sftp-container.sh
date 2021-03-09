db9f0c9439dde02303d502b51b4dd7c6f44f08b7
Thomas Schersach <tschersach@gmail.com>
2021-03-09 18:26:04 +0100

    try fixing ci

--- create-sftp-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -eux
-/usr/bin/docker create  -p 2222:22  -e "SFTP_USERS=sftp:sftp:::upload" -e GITHUB_ACTIONS=true -e CI=true -v "/$(pwd)/test/__fixtures__/samples":"/home/sftp/samples" atmoz/sftp
-sleep 10
